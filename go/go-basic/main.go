272947b1ab4c0d5241bdf572c87bece7126449a2
castillo9x <castillo.randolfo@gmail.com>
2022-07-21 23:30:44 -0300

    feat: Go Basic

---
+++ go/go-basic/main.go
@@ -0,0 +1,13 @@
+// Task 1 Definition de package
+package main
+
+// Task Set Library
+import "fmt"
+
+// Task: Entry Point
+func main() {
+	var greeting string = "Go language"
+
+	// Task: Console
+	fmt.Println(greeting)
+}
