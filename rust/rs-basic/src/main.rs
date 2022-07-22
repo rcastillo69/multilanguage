b63d86598045a33824156c5eeb1540c6d29d3c20
castillo9x <castillo.randolfo@gmail.com>
2022-07-21 23:05:24 -0300

    feat: Rust Basic

---
+++ rust/rs-basic/src/main.rs
@@ -0,0 +1,10 @@
+// Task 1. This is a comment
+
+// Task : This is a main. Entry point program
+fn main() {
+
+    let greeting = "Welcome to rust";
+
+    // Task 3: Message by console
+    println!("{}", greeting.to_string());
+}
