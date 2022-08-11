9ef01543ec9d893cf704e1f281160f14b8802555
castillo9x <castillo.randolfo@gmail.com>
2022-08-11 01:47:02 -0300

    feat: update imperative

--- rust/rs-basic/src/main.rs
+++ rust/rs-basic/src/main.rs
@@ -1,10 +1,13 @@
-// Task 1. This is a comment
+// Task: This is a single-line comment
+
+/*
+   Task: This is a multi-line comment
+
+*/
 
 // Task : This is a main. Entry point program
 fn main() {
-
     let greeting = "Welcome to rust";
-
-    // Task 3: Message by console
+    // Task: Message by console
     println!("{}", greeting.to_string());
 }
--- rust/rs-imperative/src/main.rs
+++ rust/rs-imperative/src/main.rs
@@ -13,9 +13,83 @@ fn main(){
     println!("User limit: {}", USER_LIMIT );
     println!("User min: {}", USER_MIN );
 
-/*
-    print!("Print {}", "Hi\n");
+    // Task define integer 32
+    let x:i32 = 21;
+    println!("type of x {}", type_of(&x));
+    println!("variable x {}", x);
 
+    // Task define integer 64
+    let x64:i64 = 64;
+    println!("type of x64 {}", type_of(&x64));
+    println!("variable x64 {}", x);
+
+    // Task define a unsigned integer
+    let xu8:u8 = 8;
+    let xu16:u16 = 16;
+    let xu32:u32 = 32;
+    let xu64:u64 = 64;
+    let xu128:u128 = 132;
+
+    // Task define a unsigned integer
+    let xf32:f32 = 32.0;
+    let xf64:f64 = 64.0;
+    println!("type of xf32 {}", type_of(&xf32));
+    println!("variable xf32 {}", xf32);
+    println!("type of xf64 {}", type_of(&xf64));
+    println!("variable xf64 {}", xf64);
+
+    // Task define a string
+    // String literal
+    let company:&str="My company";
+    let location:&str = "Australia";
+    println!("Company is : {} Location :{}",company,location);
+
+    let company2:&'static str = "Company";
+    let location2:&'static str = "Argentina";
+    println!("Company2 is : {} Location2 :{}",company2,location2);
+
+    // String object
+    let empty_string = String::new();
+    let content_string = String::from("Rust is beatiful");
+    println!("empty_string the length is {}",empty_string.len());
+    println!("content_string the length is {}",content_string.len());
+
+    // Task Char
+    let letter:char = 'a';
+    println!("type of letter {}", type_of(&letter));
+    println!("variable letter {}", letter);
+
+    // Task Concatenation
+    let mut greeting = "Hi ".to_string();
+    greeting.push_str("Rust");
+    println!("greeting = {}",greeting);
+
+    // Task boolean
+    let isValid:bool = true;
+    println!("type of isValid {}", type_of(&isValid));
+    println!("variable isValid {}", isValid);
+
+    // Task Enum
+    // The `derive` attribute automatically creates the implementation
+    // required to make this `enum` printable with `fmt::Debug`.
+    #[derive(Debug)]
+    enum GenderCategory {
+        Male,Female
+    }
+
+    let male = GenderCategory::Male;
+    let female = GenderCategory::Female;
+    println!("{:?}",male);
+
+    let x = 5;
+    let raw = &x as *const i32;
+    let points_at = unsafe { *raw };
+    println!("raw points at {}", points_at);
+
+
+
+
+/*
     // Print with parameters
     println!("{0}, this is {1}. {1}, this is {0} \n", "Alice", "Bob");
 
@@ -35,10 +109,7 @@ fn main(){
 
 
 
-    // Task define integer
-    let x = 21;
-    println!("type of x {}", type_of(&x));
-    println!("variable x {}", x);
+
 
     let y = 2.5;
     let s ="string1";
