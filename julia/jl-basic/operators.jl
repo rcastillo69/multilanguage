08ae24b98053d0fb35f3e9959142141ecb17e2e6
rcastillo <castillo.randolfo@gmail.com>
2023-05-30 23:17:28 -0300

    add operators

---
+++ julia/jl-basic/operators.jl
@@ -0,0 +1,143 @@
+# Operatos
+
+#=
+  Task Arithmetic operators
+=#
+function show_arithmetic_operators()
+println("* Aritmentic operators")
+# Addition
+result = 5 + 3
+println(result)  # Output: 8
+
+# Subtraction
+result = 10 - 4
+println(result)  # Output: 6
+
+# Multiplication
+result = 2 * 6
+println(result)  # Output: 12
+
+# Division
+result = 15 / 4
+println(result)  # Output: 3.75
+
+# Modulus (Remainder)
+result = 17 % 5
+println(result)  # Output: 2
+
+# Increment
+num = 5
+num += 1
+println(num)  # Output: 6
+
+# Decrement
+num = 8
+num -= 1
+println(num)  # Output: 7
+    
+end
+
+# Task: Assignment operators:
+function show_assignmentc_operators()
+
+println("* Assignment operators")
+# Assignment operator
+x = 10
+println(x)  # Output: 10
+
+# Compound assignment operators
+y = 5
+y += 2
+println(y)  # Output: 7
+
+z = 10
+z *= 3
+println(z)  # Output: 30
+
+w = 15
+w /= 2
+println(w)  # Output: 7.5
+
+m = 10
+m %= 3
+println(m)  # Output: 1
+
+end
+
+#Task: Comparison operators
+function show_comparison_operator()
+
+println("* Comparison operators")
+# Equality
+result = (5 == 5)
+println(result)  # Output: True
+
+# Inequality
+result = (7 != 3)
+println(result)  # Output: True
+
+# Greater than
+result = (10 > 8)
+println(result)  # Output: True
+
+# Less than
+result = (6 < 4)
+println(result)  # Output: False
+
+# Greater than or equal to
+result = (5 >= 5)
+println(result)  # Output: True
+
+# Less than or equal to
+result = (3 <= 2)
+println(result)  # Output: False
+
+end
+
+#Task: Comparison operators
+function show_logical_operators()
+
+println("* Logical operators")    
+# Logical AND
+result = true && false
+println(result)  # Output: false
+
+# Logical OR
+result = true || false
+println(result)  # Output: true
+
+# Logical NOT
+result = !true
+println(result)  # Output: false
+
+   
+end
+
+#Task: Concatenational operator 
+function show_concatenation_operator()
+
+println("* Concatenation operator")    
+# String concatenation
+greeting = "Hello, "
+name = "John"
+message = greeting * name
+println(message)  # Output: Hello, John
+
+end
+
+#Task: Ternary conditional operator
+function show_ternary_concatenation_operator()
+
+    println("* Ternary conditional operator")    
+    x = 10
+    result = (x % 2 == 0) ? "Even" : "Odd"
+    println(result)  # Output: Even
+    
+ end
+
+show_arithmetic_operators() 
+show_assignmentc_operators()
+show_comparison_operator()
+show_logical_operators()
+show_concatenation_operator()
+show_ternary_operator()
---
+++ python/py-basic/operator.py
@@ -0,0 +1,115 @@
+
+"""
+    Types of operators
+"""
+#Task Aritmetic Operators
+def show_aritmetic_operators():
+    
+    # Addition
+    result = 5 + 3
+    print(result)  # Output: 8
+
+    # Subtraction
+    result = 10 - 4
+    print(result)  # Output: 6
+
+    # Multiplication
+    result = 2 * 6
+    print(result)  # Output: 12
+
+    # Division
+    result = 15 / 4
+    print(result)  # Output: 3.75
+
+    # Modulus (Remainder)
+    result = 17 % 5
+    print(result)  # Output: 2
+
+    # Increment
+    num = 5
+    num += 1
+    print(num)  # Output: 6
+
+    # Decrement
+    num = 8
+    num -= 1
+    print(num)  # Output: 7
+    
+
+#Task Assignment operators
+def show_assigment_operators():
+    # Assignment operator
+    x = 10
+    print(x)  # Output: 10
+
+    # Compound assignment operators
+    y = 5
+    y += 2
+    print(y)  # Output: 7
+
+    z = 10
+    z *= 3
+    print(z)  # Output: 30
+
+    w = 15
+    w /= 2
+    print(w)  # Output: 7.5
+
+    m = 10
+    m %= 3
+    print(m)  # Output: 1
+
+#Task: Comparison operators:
+def show_comparison_operators():
+    # Equality
+    result = (5 == 5)
+    print(result)  # Output: True
+
+    # Inequality
+    result = (7 != 3)
+    print(result)  # Output: True
+
+    # Greater than
+    result = (10 > 8)
+    print(result)  # Output: True
+
+    # Less than
+    result = (6 < 4)
+    print(result)  # Output: False
+
+    # Greater than or equal to
+    result = (5 >= 5)
+    print(result)  # Output: True
+
+    # Less than or equal to
+    result = (3 <= 2)
+    print(result)  # Output: False
+
+# Task:  Logical operators
+def show_logical_operators():
+    # Logical AND
+    result = True and False
+    print(result)  # Output: False
+
+    # Logical OR
+    result = True or False
+    print(result)  # Output: True
+
+    # Logical NOT
+    result = not True
+    print(result)  # Output: False
+
+#Task: Conditional operators
+def show_conditional_operator():
+    # Ternary conditional operator
+    x = 10
+    result = "Even" if x % 2 == 0 else "Odd"
+    print(result)  # Output: Even
+
+
+if __name__ == "__main__":
+    show_aritmetic_operators()
+    show_assigment_operators()
+    show_comparison_operators()
+    show_logical_operators()
+    show_conditional_operator()
