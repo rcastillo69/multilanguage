b456e7d7673b3725252a313f11e79f845a6fa48b
rcastillo <castillo.randolfo@gmail.com>
2023-06-26 21:57:29 -0300

    review operatos, input & refacto folder

---
+++ python/fundamentals/input.py
@@ -0,0 +1,12 @@
+# Recive a input data
+
+
+#Task Entry point
+def get_input():
+    name = input("what is your name? ")
+    
+    #Output console
+    print("Hello, ", name)
+
+if __name__ == "__main__":
+    get_input()
--- python/fundamentals/operatortypes.py
+++ python/fundamentals/operatortypes.py
@@ -19,7 +19,11 @@ def show_aritmetic_operators():
 
     # Division
     result = 15 / 4
-    print(result)  # Output: 3.75
+    print("Division;",result)  # Output: 3.75
+    
+    # Floor division or integer division. Quotient when a is divided by b, rounded to the next smallest whole number
+    a= 15//4
+    print ("Floor division",a) # Output: 3
 
     # Modulus (Remainder)
     result = 17 % 5
@@ -35,6 +39,17 @@ def show_aritmetic_operators():
     num -= 1
     print(num)  # Output: 7
     
+    # Exponential. Form 1
+    base = 3
+    exponent = 4
+    print("Exponential Value is: ", base ** exponent)
+    
+    # Exponential. Form 2
+    base = 2
+    exponent = 4
+    print("Exponential Value is: ", pow(base, exponent))
+    
+
 
 #Task Assignment operators
 def show_assigment_operators():
@@ -106,10 +121,20 @@ def show_conditional_operator():
     result = "Even" if x % 2 == 0 else "Odd"
     print(result)  # Output: Even
 
+#Task: Concatenate String
+def show_concatenante_string():
+    # string definition
+    x = "This is a string"
+    y = " This is a string2"
+    result = x + y
+    print(result)  # Output: Even
+
 
 if __name__ == "__main__":
     show_aritmetic_operators()
     show_assigment_operators()
     show_comparison_operators()
     show_logical_operators()
+    show_concatenante_string()
     show_conditional_operator()
+
