035f83a38206164124c01d4746421a0554b2748c
rcastillo <tepuybits@gmail.com>
2024-05-16 00:42:26 -0300

    Problem 002

--- challenges/coding/c002/py/c002.py
+++ challenges/coding/c002/py/c002.py
@@ -1,9 +1,50 @@
-
-def solveMeFirst(a,b):
-	return a + b
+import os
 
 
-num1 = int(input())
-num2 = int(input())
-res = solveMeFirst(num1,num2)
-print(res)
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def get_militar_time(timelist:list[str]) -> str:
+    return timelist[0]+":"+timelist[1] +":"+timelist[2]
+
+def get_meridiam(s:str) -> str:
+    return s[-2:] 
+
+def get_time(s:str):
+	return s[:len(s)-2].split(":")
+
+def timeConversion(s):
+    
+	meridiam = get_meridiam(s).upper()
+      
+	timelist = get_time(s)
+
+	if meridiam == "PM" and int(timelist[0]) < 12:
+		timelist[0] = str(12 + int(timelist[0]) )      
+    
+	if meridiam == "AM" and int(timelist[0]) == 12:
+		timelist[0] = '00'
+      
+    
+	return get_militar_time(timelist)
+
+if __name__ == '__main__':
+	# Get the directory of the current script
+	script_dir = os.path.dirname(os.path.abspath(__file__))
+
+	# Define the output file path in the same directory as the script
+	output_path = os.path.join(script_dir, 'output.txt')
+    
+	fptr = open(output_path, 'w')
+
+	s = input()
+
+	result = timeConversion(s)
+
+	fptr.write(result + '\n')
+
+	fptr.close()
