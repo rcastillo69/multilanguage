3fb34bc67dd30321cd4ebfb5f612ac0ff8e2cef6
rcastillo <tepuybits@gmail.com>
2024-06-04 12:28:52 -0300

    Update py c001

--- challenges/coding/c002/py/c002.py
+++ challenges/coding/c002/py/c002.py
@@ -1,50 +1,32 @@
-import os
 
 
-#
-# Complete the 'timeConversion' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING s as parameter.
-#
+def get_military_time(parts: list[str]) -> str:
+    return parts[0] + ":" + parts[1] + ":" + parts[2]
 
-def get_militar_time(timelist:list[str]) -> str:
-    return timelist[0]+":"+timelist[1] +":"+timelist[2]
 
-def get_meridiam(s:str) -> str:
-    return s[-2:] 
+def get_meridian(s: str) -> str:
+    return s[-2:]
 
-def get_time(s:str):
-	return s[:len(s)-2].split(":")
+
+def get_time(s: str) -> list[str]:
+    return s[:len(s) - 2].split(":")
+
 
 def timeConversion(s):
-    
-	meridiam = get_meridiam(s).upper()
-      
-	timelist = get_time(s)
+    meridian = get_meridian(s).upper()
 
-	if meridiam == "PM" and int(timelist[0]) < 12:
-		timelist[0] = str(12 + int(timelist[0]) )      
-    
-	if meridiam == "AM" and int(timelist[0]) == 12:
-		timelist[0] = '00'
-      
-    
-	return get_militar_time(timelist)
+    parts_time = get_time(s)
+
+    if meridian == "PM" and int(parts_time[0]) < 12:
+        parts_time[0] = str(12 + int(parts_time[0]))
+
+    if meridian == "AM" and int(parts_time[0]) == 12:
+        parts_time[0] = '00'
+
+    return get_military_time(parts_time)
+
 
 if __name__ == '__main__':
-	# Get the directory of the current script
-	script_dir = os.path.dirname(os.path.abspath(__file__))
+    s = input("Input a datetime:")
 
-	# Define the output file path in the same directory as the script
-	output_path = os.path.join(script_dir, 'output.txt')
-    
-	fptr = open(output_path, 'w')
-
-	s = input()
-
-	result = timeConversion(s)
-
-	fptr.write(result + '\n')
-
-	fptr.close()
+    print(timeConversion(s))
